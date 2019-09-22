30440403a47a526d7e2178b764d1d5de0aa6cfa3
Tomm Huth <tomm.huth@makingwaves.com>
2019-09-22 17:42:19 +0200

    rewording

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ import Config from "./Config"
 ReactDOM.render(
     <>
         <h1 className="visually-hidden">Blood sugar measurement converter</h1>
-        <p className="visually-hidden">Convert from/to <em>mmol/L</em> and <em>mg/dL</em>.</p>
+        <p className="visually-hidden">Convert between <em>mmol/L</em> and <em>mg/dL</em>.</p>
         <p className="visually-hidden">The international standard way of measuring blood glucose levels is in terms of a molar concentration, measured in mmol/L (millimoles per litre; or millimolar, abbreviated mM). In the United States, Germany and other countries mass concentration is measured in mg/dL (milligrams per decilitre).</p>
         
         <Converter />
